d5b8c81e6810290855bb03756930e39dececee6d
Bernardo Garcia Fensterseifer <bernardofens@10-41-62-126.0200.epoa.br.iosda.org>
2025-05-13 17:28:47 -0300

    2componentsByBernardo

---
+++ purrchase/Components/ImageCatComponent.swift
@@ -0,0 +1,81 @@
+//
+//  ImageCatComponent.swift
+//  purrchase
+//
+//  Created by Bernardo Garcia Fensterseifer on 13/05/25.
+//
+
+import UIKit
+
+class ImageCatComponent: UIView {
+    
+    // MARK: Subviews
+    private lazy var nameLabel: UILabel = {
+        var label = UILabel()
+        label.font = UIFont(name: "Quicksand-Regular", size: 17)
+        label.text = "Click on “Add List” to add a new list"
+        return label
+    }()
+    
+    lazy var catImage : UIImageView  = {
+        let icon = UIImageView()
+        icon.translatesAutoresizingMaskIntoConstraints = false
+        icon.image = UIImage(named: "Cat-Image1")
+        icon.heightAnchor.constraint(equalToConstant: 215).isActive = true
+        icon.widthAnchor.constraint(equalToConstant: 215).isActive = true
+        return icon
+    }()
+
+    private lazy var stack: UIStackView = {
+        var stack = UIStackView(arrangedSubviews: [catImage, nameLabel])
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.spacing = 0
+        return stack
+    }()
+    
+    // MARK: Properties
+    var name: String? {
+        didSet {
+            nameLabel.text = name
+        }
+    }
+    
+    var image: UIImage? {
+        get {
+            return catImage.image
+        }
+        set {
+            catImage.image = newValue
+        }
+    }
+    
+    // MARK: Initializers
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+extension ImageCatComponent: ViewCodeProtocol {
+    
+    func addSubviews() {
+        addSubview(stack)
+    }
+    
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            stack.topAnchor.constraint(equalTo: self.topAnchor),
+            stack.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+            stack.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            stack.trailingAnchor.constraint(equalTo: self.trailingAnchor)
+            /// deixei comentado pois acredito que talvez a designer queira que essa constraint mude dependendo do dispositivo (ex.: iPad)
+            //  stack.heightAnchor.constraint(equalToConstant: 257)
+        ])
+    }
+    
+}
