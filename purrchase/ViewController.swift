5e847990de0d3763ba41726c909e3e8eb33b82ca
Richard Fagundes Rodrigues <richardsiacc@Richards-MacBook-Pro.local>
2025-05-13 14:14:05 -0300

    terceiro teste de push

--- purrchase/ViewController.swift
+++ purrchase/ViewController.swift
@@ -13,7 +13,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    let teste: String = "tetando git"
 
-
+    
 }
 
