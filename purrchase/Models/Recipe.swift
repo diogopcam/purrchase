43fe2863010deda2f496120ab48d154c589ebb47
Diogo Pessin Ca <diogpcam@Diogo-Camargo---MacBook-Pro.local>
2025-05-14 16:55:30 -0300

    Criacao do modelo Recipe.

--- purrchase/Models/Recipe.swift
+++ purrchase/Models/Recipe.swift
@@ -4,4 +4,20 @@
 //
 //  Created by Diogo Camargo on 14/05/25.
 //
+import UIKit
+import Foundation
 
+final class Recipe: Codable {
+    var id: UUID = UUID()
+    var name: String
+    var image: String
+    var ingredients: [Product]
+    var steps: [String]
+    
+    init(name: String, image: String, ingredients: [Product], steps: [String]) {
+        self.name = name
+        self.image = image
+        self.ingredients = ingredients
+        self.steps = steps
+    }
+}
